898dca42f0f2f5a440064756595b79a12268b431
Abdul Kuddus <55076423+abdulkuddus225@users.noreply.github.com>
2019-10-20 12:43:34 +0530

    Add files via upload

---
+++ assignment/gitsearch/views.py
@@ -0,0 +1,20 @@
+
+from django.shortcuts import render
+from django.conf import settings
+
+import requests
+from github import Github
+
+from .forms import SubmitGit
+from .serializer import GitSerializer
+from github import Github, GithubException
+
+def GitSearch(request):
+    user = {}
+    if 'username' in request.GET:
+        username = request.GET['username']
+        url = 'https://api.github.com/users/%s' % username
+        response = requests.get(url)
+        user = response.json()
+        print(user)
+    return render(request, 'index.html', {'search_result': user})
